3e03fee8ed7436b8b1676d3ff09d0285584ca07d
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-03-10 21:44:01 -0300

    feat: Criando tela de confirmação do envio após o  subimit

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -2,16 +2,18 @@
 
 import { Routes, Route } from "react-router";
 
-import { AppLayout } from "../components/AppLayout";
-
 import { Refund } from "../pages/Refund";
 import { NotFound } from "../pages/Notfound";
+import { Confirm } from "../pages/confirm";
+
+import { AppLayout } from "../components/AppLayout";
 
 export function EmployeeRoutes() {
     return (
         <Routes>
             <Route path="/" element={<AppLayout />}>
                 <Route path="/" element={<Refund/>}/>
+                <Route path="/Confirm" element={<Confirm/>}/>
             </Route>
 
             <Route path="*" element={<NotFound/>}/>
