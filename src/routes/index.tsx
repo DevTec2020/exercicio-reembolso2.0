4ecda4672eac0956e742f5a24beb3a6eb6d406fe
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-03-14 20:43:13 -0300

    feat: criando component de loading após o login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading.tsx";
+
 import { AuthRoutes } from "./AuthRoutes.tsx";
 import { ManagerRoutes } from "./ManagerRoutes.tsx";
 import { EmployeeRoutes } from "./EmployeeRoutes.tsx";
 
+const isLoading = false
+
 export function Routes(){
+    if (isLoading) {
+        return <Loading/>
+    }
+
     return (
         <BrowserRouter>
-            <ManagerRoutes />
+            <AuthRoutes />
         </BrowserRouter>
     )
 }
