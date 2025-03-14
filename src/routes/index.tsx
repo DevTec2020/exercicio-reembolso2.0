6a09e897d76c1c9f55e03d755eaf534b5802095e
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-03-14 20:53:36 -0300

    feat: Gerenciando rotas de acordo com o usuário logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,33 @@ import { EmployeeRoutes } from "./EmployeeRoutes.tsx";
 
 const isLoading = false
 
+const session = {
+    user: {
+        role: "manager",
+    },
+}
+
 export function Routes(){
+    function Route() {
+        switch (session.user.role) {
+            case "employee":
+                return  <EmployeeRoutes />     
+                       
+            case "manager":
+                return <ManagerRoutes/>
+
+            default:
+                return <AuthRoutes/>
+        }
+    }
+    
     if (isLoading) {
         return <Loading/>
     }
 
     return (
         <BrowserRouter>
-            <AuthRoutes />
+            {Route()}
         </BrowserRouter>
     )
 }
