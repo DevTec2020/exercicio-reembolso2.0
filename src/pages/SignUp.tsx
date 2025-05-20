7d684083e67452f6be37383f77e0e82dff47bcd9
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-05-20 18:59:03 -0300

    feat: Adicionando zod e validação dos campos do form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,21 @@
 import { useState } from "react"
+import { z, ZodError } from "zod"
 
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+
+const singUpSchema = z.object({
+    name: z.string().trim().min(1, {message: "Informe o nome"}),
+    email: z.string().email({message: "E-mail Invalido"}),
+    password: z
+        .string()
+        .min(6, {message: "Senha deve ter pelo menos 6 digitos"}),
+    passwordConfirm: z.string({message: "Confirme a senha"}),
+}) .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+})
+
 
 export function SignUp(){
     const [name, setName] = useState("")
@@ -13,7 +27,26 @@ export function SignUp(){
     function onSubmit(e: React.FormEvent) {
         e.preventDefault()
 
-        console.log(name, email, password, passwordConfirm)
+        try {
+            setIsLoading(true)
+
+            const data = singUpSchema.parse({
+                name,
+                email,
+                password,
+                passwordConfirm,
+            })
+
+        } catch (error) {
+          if (error instanceof ZodError) {
+            return alert(error.issues[0].message)
+          }  
+
+          alert("Não foi possivel cadastrar!")
+
+        }finally {
+            setIsLoading(false)
+        }
     }
 
     return (
