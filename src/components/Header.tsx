0416d04a0ea367ed3fc853726d60b0c031c6c3ee
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-05-23 21:32:18 -0300

    feat: criando funão de sair do sistema

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import logosvg from "../assets/logo.svg"
 import logoutsvg from "../assets/logout.svg"
 
+import { useAuth } from "../hooks/useAuth"
+
 export function Header() {
+    const auth  = useAuth()
+
     return (
         <header className="w-full flex justify-between">
             <img src={logosvg} alt="Logo" className="my-8"/>
@@ -15,6 +19,7 @@ export function Header() {
                     src={logoutsvg} 
                     alt="icone de sair" 
                     className="my-8 cursor-pointer hover:opacuty-75 transition ease-linear"
+                    onClick={() => auth.remove()}
                 />
             </div>
         </header>
