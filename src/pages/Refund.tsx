b5bb29c7884f9a307142b33cff0eaece416bb1da
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-05-25 18:59:42 -0300

    feat: Criando validação everificação do form com zod

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,6 +1,7 @@
 // Pagina de colaborador
 import { useState } from "react"
 import { useNavigate, useParams } from "react-router"
+import { z, ZodError} from "zod"
 
 import fileSvg from "../assets/file.svg"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
@@ -10,6 +11,16 @@ import { Select } from "../components/Select"
 import { Upload } from "../components/Upload"
 import { Button } from "../components/Button"
 
+
+const refundSchema = z.object({
+    name: z
+        .string()
+        .min(3, {message: "Informe um nome claro para sua solicitação "}),
+    category: z.string().min(1, {message: "Informe a Categoria"}),
+    amount: z.coerce
+        .number({message: "Informe um valor Valido"})
+        .positive({message: "informe um valor válido e superior a 0"}),    
+})
 
 
 export function Refund() {
@@ -30,8 +41,28 @@ export function Refund() {
             return navigate(-1)
         }
 
-        console.log(name, amount, category, filename)
-        navigate("/confirm", {state: {fromSubmit: true}})
+        try {
+            setIsLoading (true)
+
+            const data = refundSchema.parse({
+                name,
+                category,
+                amount: amount.replace(",","."),
+            })
+
+            console.log(data)
+            //navigate("/confirm", {state: {fromSubmit: true}})
+        } catch (error) {
+            console.log(error)
+            
+            if(error instanceof ZodError) {
+                return alert(error.issues[0].message)
+            }
+
+            alert("Não foi possivel realizar a solicitação")
+        } finally {
+            setIsLoading(false)
+        }
     }
 
 
